5f7ffb4863fa36e6e5c3f10f31ab2dd7ffb4c6f1
Alex Mathew <alexmathew003@gmail.com>
2014-10-01 00:01:55 +0530

    Initial commit of scraper

---
+++ scrape.py
@@ -0,0 +1,53 @@
+import requests
+from lxml import etree
+from functools import wraps
+import os
+import urlparse
+import time
+import psycopg2
+
+
+def connectDB(wrapped):
+    @wraps(wrapped)
+    def inner(*args, **kwargs):
+        urlparse.uses_netloc.append("postgres")
+        url = urlparse.urlparse(os.environ["DATABASE_URL"])
+        conn = psycopg2.connect(
+            database=url.path[1:],
+            user=url.username,
+            password=url.password,
+            host=url.hostname,
+            port=url.port
+        )
+        cur = conn.cursor()
+        ret = wrapped(conn, cur, *args, **kwargs)
+        conn.commit()
+        cur.close()
+        conn.close()
+        return ret
+    return inner
+
+
+@connectDB
+def scrape(*args):
+    conn, cur = args[:1]
+    genre_page = etree.HTML(requests.get('http://www.imdb.com/genre').content)
+    genres = [x.lower() for x in genre_page.xpath('//table[@class="splash"]//a/text()')]
+    url = 'http://www.imdb.com/search/title?at=0&genres=<g>&sort=user_rating&start=<s>&title_type=feature'
+    for genre in genres:
+        url2 = url.replace('<g>', genre)
+        for start in xrange(1, 751, 50):
+            page_url = url2.replace('<s>', str(start))
+            page = etree.HTML(requests.get(page_url).content)
+            movies = page.xpath('//td[@class="title"]/a/text()')
+            for movie in movies:
+                try:
+                    cur.execute('INSERT INTO MOVIES (TITLE) VALUES (%s)', (movie,))
+                except psycopg2.IntegrityError:
+                    pass
+            conn.commit()
+            time.sleep(1)
+        time.sleep(2.5)
+
+
+scrape()
